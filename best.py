b0cc2489417dd56ab06c2b3ca7dc3f82b9dc6474
Aaron Cronin <cronin@include.cat>
2013-10-20 00:37:39 +0100

    Heaps

--- best.py
+++ best.py
@@ -4,6 +4,8 @@ from collections import defaultdict
 from os import linesep
 from bisect import bisect_left, bisect_right
 import string
+from heapq import heappush, heappop
+import gc
 
 queries = FileReader('./qrys.txt').all()
 documents = FileReader('./docs.txt').all()
@@ -11,17 +13,17 @@ documents = FileReader('./docs.txt').all()
 class BestScorer:
   def __init__(self, queries, documents, filename):
     self.filename                = filename
-    self.posting                 = defaultdict(set)
+    self.posting                 = defaultdict(list)
     self.queries, self.documents = queries, documents
 
   def build_matching_vectors(self):
-    self.posting = defaultdict(list)
     for (doc_id, content) in self.documents:
       for word in content: self.posting[word].append(doc_id)
     return self
 
   def output_scores(self):
     def recent_matches(terms, num_matches):
+      num_terms = len(terms)
       def do_linear_merge(queries):
         min_max  = min((self.posting[query][-1] for query in queries))
         max_min  = max((self.posting[query][0] for query in queries))
@@ -31,16 +33,24 @@ class BestScorer:
           right = bisect_right(posting, min_max)
           return set(posting[left:right])
 
-        postings = (set_in_range(self.posting[query]) for query in queries)
-        documents = sorted(reduce(lambda x,y: x&y, sorted(postings, key=len)), reverse=True)
+        postings = ((len(p), p) for p in (set_in_range(self.posting[query]) for query in queries))
+
+        h = []
+        for p in postings: heappush(h, p)
+
+        def gen_heap(h):
+          return (heappop(h)[1] for i in xrange(num_terms))
+
+        documents = sorted(reduce(lambda x,y: x&y, gen_heap(h)), reverse=True)
         for doc in documents:
           yield str(doc)
         return
 
-      return islice(do_linear_merge(q), num_matches)
+      return islice(do_linear_merge(terms), num_matches)
 
     with open(self.filename, 'w') as docs_top:
-      for (q_n, q) in queries:
-        docs_top.write(str(q_n) + ' ' + string.join(recent_matches(q, 5), ' ') + linesep)
+      _join = string.join
+      docs_top.write(_join((str(q_n) + ' ' + _join(recent_matches(q, 5), ' ') + linesep for (q_n, q) in queries), ''))
 
+gc.disable()
 BestScorer(queries, documents, './best.top').build_matching_vectors().output_scores()
--- file_reader.py
+++ file_reader.py
@@ -8,17 +8,11 @@ class FileReader:
       if should_reverse: self.lines.reverse()
 
   def all(self):
-    lowers = string.ascii_lowercase + ' '
-    digits = string.digits
-
-    letter_set = frozenset(lowers + digits)
-
-    deletions = ''.join(ch for ch in map(chr,range(256)) if ch not in letter_set)
 
     tab = string.maketrans(
-      lowers + digits + deletions,
-      lowers + digits + (' ' * len(deletions))
+      string.ascii_uppercase + string.punctuation,
+      string.ascii_lowercase + (' ' * len(string.punctuation))
     )
 
     _ts = string.translate
-    return ((int(split[0]), frozenset(split[1:])) for split in (_ts(line.lower(), tab).split() for line in self.lines) )
+    return ((int(split[0]), frozenset(split[1:])) for split in (_ts(line, tab).split() for line in self.lines) )
