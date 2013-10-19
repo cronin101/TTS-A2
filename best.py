3430a905f23c3b14bb8bf3a2f3be522f36f4196c
Aaron Cronin <cronin@include.cat>
2013-10-19 23:35:29 +0100

    genexp

--- best.py
+++ best.py
@@ -6,7 +6,7 @@ from bisect import bisect_left, bisect_right
 import string
 
 queries = FileReader('./qrys.txt').all()
-documents = list(FileReader('./docs.txt').all())
+documents = FileReader('./docs.txt').all()
 
 class BestScorer:
   def __init__(self, queries, documents, filename):
@@ -21,7 +21,7 @@ class BestScorer:
     return self
 
   def output_scores(self):
-    def recent_matches(terms, documents, num_matches):
+    def recent_matches(terms, num_matches):
       def do_linear_merge(queries):
         min_max  = min((self.posting[query][-1] for query in queries))
         max_min  = max((self.posting[query][0] for query in queries))
@@ -41,6 +41,6 @@ class BestScorer:
 
     with open(self.filename, 'w') as docs_top:
       for (q_n, q) in queries:
-        docs_top.write(str(q_n) + ' ' + string.join(recent_matches(q, self.documents, 5), ' ') + linesep)
+        docs_top.write(str(q_n) + ' ' + string.join(recent_matches(q, 5), ' ') + linesep)
 
 BestScorer(queries, documents, './best.top').build_matching_vectors().output_scores()
